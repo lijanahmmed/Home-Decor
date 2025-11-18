257067dbc25ca3e7ffb01d645830c40ab6da8bd3
lijanahmmed <lijanahmmed@gmail.com>
2025-11-18 18:48:18 +0600

    added wishlist section

--- src/Pages/Wishlist.jsx
+++ src/Pages/Wishlist.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import {
   Bar,
   BarChart,
@@ -8,87 +8,85 @@ import {
   Tooltip,
   XAxis,
   YAxis,
-} from 'recharts'
-import { loadWishlist, removeFromWishlist } from '../utils/localStorage'
+} from "recharts";
+import { loadWishlist, removeFromWishlist } from "../utils/localStorage";
 
 const Wishlist = () => {
-  const [wishlist, setWishlist] = useState(() => loadWishlist())
-  const [sortOrder, setSortOrder] = useState('none')
+  const [wishlist, setWishlist] = useState(() => loadWishlist());
+  const [sortOrder, setSortOrder] = useState("none");
 
-  if (!wishlist.length) return <p>No Data Available</p>
+  if (!wishlist.length) return <p>No Data Available</p>;
 
   const sortedItem = (() => {
-    if (sortOrder === 'price-asc') {
-      return [...wishlist].sort((a, b) => a.price - b.price)
-    } else if (sortOrder === 'price-desc') {
-      return [...wishlist].sort((a, b) => b.price - a.price)
+    if (sortOrder === "price-asc") {
+      return [...wishlist].sort((a, b) => a.price - b.price);
+    } else if (sortOrder === "price-desc") {
+      return [...wishlist].sort((a, b) => b.price - a.price);
     } else {
-      return wishlist
+      return wishlist;
     }
-  })()
+  })();
 
-  const handleRemove = id => {
-    // remove from localstorage
-    removeFromWishlist(id)
-    // for ui instant update
-    setWishlist(prev => prev.filter(p => p.id !== id))
-  }
+  const handleRemove = (id) => {
+    removeFromWishlist(id);
+    setWishlist((prev) => prev.filter((p) => p.id !== id));
+  };
 
   //   generate chart data
-  const totalsByCategory = {}
-  wishlist.forEach(product => {
-    const category = product.category
+  const totalsByCategory = {};
+  wishlist.forEach((product) => {
+    const category = product.category;
 
     totalsByCategory[category] =
-      (totalsByCategory[category] || 0) + product.price
-  })
-  const chartData = Object.keys(totalsByCategory).map(category => ({
+      (totalsByCategory[category] || 0) + product.price;
+  });
+  const chartData = Object.keys(totalsByCategory).map((category) => ({
     category,
     total: totalsByCategory[category],
-  }))
-  console.log(chartData)
+  }));
+  console.log(chartData);
 
   return (
-    <div className='space-y-6'>
-      <div className='flex justify-between py-5 items-center'>
-        <h1 className='text-3xl font-semibold'>
-          Wishlist{' '}
-          <span className='text-sm text-gray-500'>
+    <div className="space-y-6">
+      <div className="flex justify-between py-5 items-center">
+        <h1 className="text-3xl font-semibold">
+          Wishlist{" "}
+          <span className="text-sm text-gray-500">
             ({sortedItem.length}) Products Found.
           </span>
         </h1>
 
-        <label className='form-control w-full max-w-xs'>
+        <label className="form-control w-full max-w-xs">
           <select
-            className='select select-bordered'
+            className="select select-bordered"
             value={sortOrder}
-            onChange={e => setSortOrder(e.target.value)}
+            onChange={(e) => setSortOrder(e.target.value)}
           >
-            <option value='none'>Sort by price</option>
-            <option value='price-asc'>Low-&gt;High</option>
-            <option value='price-desc'>High-&gt;Low</option>
+            <option value="none">Sort by price</option>
+            <option value="price-asc">Low-&gt;High</option>
+            <option value="price-desc">High-&gt;Low</option>
           </select>
         </label>
       </div>
-      <div className='space-y-3'>
-        {sortedItem.map(p => (
-          <div key={p.id} className='card card-side bg-base-100 shadow border'>
+      <div className="space-y-3">
+        {sortedItem.map((p) => (
+          <div key={p.id} className="card card-side bg-base-100 shadow border">
             <figure>
               <img
-                className='w-40 h-28 object-cover'
+                className="w-40 h-28 object-cover"
                 src={p.image}
                 alt={p.name}
               />
             </figure>
-            <div className='card-body'>
-              <h3 className='card-title'>{p.name}</h3>
-              <p className='text-base-content/70'>{p.category}</p>
+            <div className="card-body">
+              <h3 className="card-title">{p.name}</h3>
+              <p className="text-base-content/70">{p.category}</p>
             </div>
-            <div className='pr-4 flex items-center gap-3'>
-              <div className='font-semibold'>${p.price}</div>
+            <div className="pr-4 flex items-center gap-3">
+              <div className="font-semibold">${p.price}</div>
               <button
                 onClick={() => handleRemove(p.id)}
-                className='btn btn-outline'
+                className="btn btn-outline"
               >
                 Remove
               </button>
@@ -97,25 +95,24 @@ const Wishlist = () => {
         ))}
       </div>
 
-      {/* chart */}
-      <div className='space-y-3'>
-        <h3 className='text-xl font-semibold'>Wishlist Summery</h3>
-        <div className='bg-base-100 border rounded-xl p-4 h-80'>
-          <ResponsiveContainer width='100%' height='100%'>
+      <div className="space-y-3">
+        <h3 className="text-xl font-semibold">Wishlist Summery</h3>
+        <div className="bg-base-100 border rounded-xl p-4 h-80">
+          <ResponsiveContainer width="100%" height="100%">
             <BarChart data={chartData}>
-              <CartesianGrid strokeDasharray='3 3' />
-              <XAxis dataKey='category' />
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="category" />
               <YAxis />
               <Tooltip />
               <Legend />
 
-              <Bar dataKey='total' fill='#82ca9d' />
+              <Bar dataKey="total" fill="#82ca9d" />
             </BarChart>
           </ResponsiveContainer>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Wishlist
+export default Wishlist;
