f2e02f70c58b727566bf8251a960a3cd6a7ee92c
lijanahmmed <lijanahmmed@gmail.com>
2025-11-18 18:53:58 +0600

    added local storage

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,35 +1,35 @@
 // get
 export const loadWishlist = () => {
   try {
-    const data = localStorage.getItem('wishlist')
-    return data ? JSON.parse(data) : []
+    const data = localStorage.getItem("wishlist");
+    return data ? JSON.parse(data) : [];
   } catch (err) {
-    console.log(err)
-    return []
+    console.log(err);
+    return [];
   }
-}
+};
 
 // save
-export const updateList = product => {
-  const wishlist = loadWishlist()
+export const updateList = (product) => {
+  const wishlist = loadWishlist();
 
   try {
-    const isDuplicate = wishlist.some(p => p.id === product.id)
-    if (isDuplicate) return alert('Already added in wishlist')
-    const updatedWishlist = [...wishlist, product]
-    localStorage.setItem('wishlist', JSON.stringify(updatedWishlist))
+    const isDuplicate = wishlist.some((p) => p.id === product.id);
+    if (isDuplicate) return alert("Already added in wishlist");
+    const updatedWishlist = [...wishlist, product];
+    localStorage.setItem("wishlist", JSON.stringify(updatedWishlist));
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-}
+};
 
 // delete
-export const removeFromWishlist = id => {
-  const wishlist = loadWishlist()
+export const removeFromWishlist = (id) => {
+  const wishlist = loadWishlist();
   try {
-    const updatedWishlist = wishlist.filter(p => p.id !== id)
-    localStorage.setItem('wishlist', JSON.stringify(updatedWishlist))
+    const updatedWishlist = wishlist.filter((p) => p.id !== id);
+    localStorage.setItem("wishlist", JSON.stringify(updatedWishlist));
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-}
+};
